04083698303c97fd84a06b2d8c5fc33317eac179
Your Name <you@example.com>
2024-08-28 22:41:26 -0400

    cleanup

--- packages/llama.cpp/server.py
+++ packages/llama.cpp/server.py
@@ -8,41 +8,41 @@
 
 # # Initialize your model here (different for each container)
 # def initialize_model():
-#     if 'llama.cpp' in subprocess.check_output(['ls']).decode():
-#         # llama.cpp
-#         global llama_model
-#         llama_model = subprocess.Popen(['./llama.cpp/main', '-m', '/models/model.gguf', '--host', '0.0.0.0', '--port', '8080'])
-#     elif 'llamafile' in subprocess.check_output(['ls']).decode():
-#         # Llamafile
-#         global llamafile_model
-#         llamafile_model = subprocess.Popen(['./llamafile', '-m', '/models/model.gguf', '--host', '0.0.0.0', '--port', '8080'])
-#     else:
-#         # MLX
-#         global mlx_model, tokenizer
-#         mlx_model = nn.TransformerDecoder.from_pretrained("/models/model")
-#         tokenizer = AutoTokenizer.from_pretrained("/models/tokenizer")
+#     if 'llama.cpp' in subprocess.check_output(['ls']).decode():
+# # llama.cpp
+#         global llama_model
+#         llama_model = subprocess.Popen(['./llama.cpp/main', '-m', '/models/model.gguf', '--host', '0.0.0.0', '--port', '8080'])
+#     elif 'llamafile' in subprocess.check_output(['ls']).decode():
+#         # Llamafile
+#         global llamafile_model
+#         llamafile_model = subprocess.Popen(['./llamafile', '-m', '/models/model.gguf', '--host', '0.0.0.0', '--port', '8080'])
+#     else:
+#         # MLX
+#         global mlx_model, tokenizer
+#         mlx_model = nn.TransformerDecoder.from_pretrained("/models/model")
+#         tokenizer = AutoTokenizer.from_pretrained("/models/tokenizer")
 
 # initialize_model()
 
 # @app.route('/generate', methods=['POST'])
 # def generate():
-#     data = request.json
-#     prompt = data['prompt']
-#     
-#     if 'llama.cpp' in subprocess.check_output(['ls']).decode() or 'llamafile' in subprocess.check_output(['ls']).decode():
-#         # For llama.cpp and Llamafile, we'll use the HTTP API they provide
-#         import requests
-#         response = requests.post('http://localhost:8080/completion', json={'prompt': prompt, 'n_predict': 100})
-#         return jsonify(response.json())
-#     else:
-#         # For MLX
-#         inputs = tokenizer(prompt, return_tensors="np")
-#         outputs = mlx_model.generate(inputs.input_ids, max_length=100)
-#         response = tokenizer.decode(outputs[0].tolist())
-#         return jsonify({'response': response})
+#     data = request.json
+#     prompt = data['prompt']
+
+#     if 'llama.cpp' in subprocess.check_output(['ls']).decode() or 'llamafile' in subprocess.check_output(['ls']).decode():
+#     # For llama.cpp and Llamafile, we'll use the HTTP API they provide
+#         import requests
+#         response = requests.post('http://localhost:8080/completion', json={'prompt': prompt, 'n_predict': 100})
+#         return jsonify(response.json())
+#     else:
+#         # For MLX
+#         inputs = tokenizer(prompt, return_tensors="np")
+#         outputs = mlx_model.generate(inputs.input_ids, max_length=100)
+#         response = tokenizer.decode(outputs[0].tolist())
+#         return jsonify({'response': response})
 
 # if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+#     app.run(host='0.0.0.0', port=5000)
 
 
 
